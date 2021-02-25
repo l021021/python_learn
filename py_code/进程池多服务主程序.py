463af5711ecb83d354f7cc856d98d549f0a97df8
l021021 <653498331@qq.com>
2021-02-25 20:19:56 +0800

    debug

--- py_code/进程池多服务主程序.py
+++ py_code/进程池多服务主程序.py
@@ -9,20 +9,17 @@ locations = [ "368307" ,"834706"  ,"234190"  ,"251092" ,"725728"  ] #yuanjin
 # locations = [ "800424" ,"155697" ] #teslian
 # locations = [ "402837" ,"952675 "  ,"268429"  ,"732449" ,"328916"  ] #huaian
 # locations = [ "897737" ,"521209"  ] #wafer
+counter=len(locations)
 def when_done(r):
-    print(r,"Job Done")
-    sys.exit(0)
+    global counter
+    counter -=1
+    if counter==0:
+        os._exit(0)
     
 if __name__ == '__main__':
-    result={}
     with ProcessPoolExecutor() as pool:
         for loc in locations:
             Future_result=pool.submit(checkSensor,loc)
-            # result[loc]= Future_result.result()
             Future_result.add_done_callback(when_done)
-            
-            # _=pool.submit(get_sensor_asset_binding,loc)
-          
+ 
         # pool.shutdown(wait=False)
-# os._exit(1)
-# EUI64-D0CF5EFFFE7931A9
